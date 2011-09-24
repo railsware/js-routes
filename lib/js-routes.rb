98ebfeb9e11a13d1c3f4528a1d6098a873070300
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-09-24 21:43:15 +0100

    make js-routes.js.erb rely on routes.rb so it's recompiled when routes are changed

--- lib/js-routes.rb
+++ lib/js-routes.rb
@@ -1,6 +1,17 @@
 require "js_routes"
 module JsRoutes
   if defined?(Rails)
-    class Engine < Rails::Engine; end
+    class Engine < Rails::Engine
+
+      JS_ROUTES_ASSET = 'js-routes'
+
+      config.after_initialize do
+        routes = Rails.root.join('config','routes.rb')
+        Rails.application.assets.register_preprocessor 'application/javascript', :routes_dependent do |ctx,data|
+          ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
+          data
+        end
+      end
+    end
   end
 end
