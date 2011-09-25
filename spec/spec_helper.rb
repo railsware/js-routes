be483b32cdd494e11b38d36f400d5117faaf6453
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-09-25 20:34:29 +0100

    require rails before js-routes as we need Rails to be defined to query its version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'js-routes'
 require 'rails/all'
+require 'js-routes'
 require "v8"
 require "active_support/core_ext/hash/slice"
 
