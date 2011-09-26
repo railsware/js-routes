1446f6d3e729e56c1f86df491e0641561eb4d89e
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-09-26 11:59:33 +0100

    factor out engine into its own file

--- lib/js-routes.rb
+++ lib/js-routes.rb
@@ -1,17 +1,2 @@
-require "js_routes"
-class JsRoutes
-  if defined?(Rails) && Rails.version >= "3.1"
-    class Engine < Rails::Engine
-
-      JS_ROUTES_ASSET = 'js-routes'
-
-      config.after_initialize do
-        routes = Rails.root.join('config','routes.rb')
-        Rails.application.assets.register_preprocessor 'application/javascript', :routes_dependent do |ctx,data|
-          ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
-          data
-        end
-      end
-    end
-  end
-end
+require 'js_routes'
+require 'js_routes/engine'
