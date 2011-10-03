e5caa1c3013ff5c61121505c4889fa999802aa9b
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-10-03 09:46:46 +0100

    fix for issue #16 - rework initializer so it fires after sprockets.environment but before the environment is finalized while here, introduce additional initializer to conveniently load configuration from config/js-routes.rb (if it exists)

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -3,12 +3,18 @@ if defined?(Rails) && Rails.version >= "3.1"
     class Engine < Rails::Engine
       JS_ROUTES_ASSET = 'js-routes'
 
-      config.after_initialize do
+      initializer 'js-routes.dependent_on_routes', :after => "sprockets.environment" do
         routes = Rails.root.join('config','routes.rb')
-        Rails.application.assets.register_preprocessor 'application/javascript', :routes_dependent do |ctx,data|
+        Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
           ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
           data
         end
+      end
+
+      initializer 'js-routes.setup', :group => :all do |app|
+        # load up the js-routes configuration file (should one exist)
+        config_file = Rails.root.join('config','js-routes.rb')
+        JsRoutes.instance_eval(IO.read(config_file)) if File.exist?(config_file)
       end
     end
   end
