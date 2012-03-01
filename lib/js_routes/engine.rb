c78c3e5db80d13b24da93f82a7c0317e26c1cd31
Casper Fabricius <me@casperfabricius.com>
2012-03-01 16:20:21 +0100

    Should also work when sprockets are not loaded, so added check for the register_preprocessor method being available

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -1,13 +1,15 @@
-if defined?(Rails)
+if defined?(Rails) && Rails.version >= "3.1"
   class JsRoutes
     class Engine < Rails::Engine
       JS_ROUTES_ASSET = 'js-routes'
 
       initializer 'js-routes.dependent_on_routes', :after => "sprockets.environment" do
-        routes = Rails.root.join('config','routes.rb')
-        Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
-          ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
-          data
+        if Rails.application.assets.respond_to?(:register_preprocessor)
+          routes = Rails.root.join('config','routes.rb')
+          Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
+            ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
+            data
+          end
         end
       end
     end
