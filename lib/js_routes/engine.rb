4dc8fda3f5a13e91668dd3506469bb5c0bff073c
Bogdan Gusiev <agresso@gmail.com>
2012-05-30 15:43:23 +0300

    Remove unnecessary version check

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -1,15 +1,13 @@
-if defined?(Rails) && Rails.version >= "3.1"
-  class JsRoutes
-    class Engine < Rails::Engine
-      JS_ROUTES_ASSET = 'js-routes'
+class JsRoutes
+  class Engine < Rails::Engine
+    JS_ROUTES_ASSET = 'js-routes'
 
-      initializer 'js-routes.dependent_on_routes', :after => "sprockets.environment" do
-        if Rails.application.assets.respond_to?(:register_preprocessor)
-          routes = Rails.root.join('config','routes.rb')
-          Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
-            ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
-            data
-          end
+    initializer 'js-routes.dependent_on_routes', :after => "sprockets.environment" do
+      if Rails.application.assets.respond_to?(:register_preprocessor)
+        routes = Rails.root.join('config','routes.rb')
+        Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
+          ctx.depend_on(routes) if ctx.logical_path == JS_ROUTES_ASSET
+          data
         end
       end
     end
