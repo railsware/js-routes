4f40830ac2c5eb6ea0269379b17deae58f2c4bdb
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-06-14 01:02:41 +0300

    fix test for rspec 3

--- spec/js_routes/generated_javascript_spec.rb
+++ spec/js_routes/generated_javascript_spec.rb
@@ -10,13 +10,13 @@ describe JsRoutes do
   describe "generated js" do
     subject { JsRoutes.generate }
     it "should have correct function without arguments signature" do
-      should include("inboxes_path: function(options)")
+      is_expected.to include("inboxes_path: function(options)")
     end
     it "should have correct function with arguments signature" do
-      should include("inbox_message_path: function(_inbox_id, _id, options)")
+      is_expected.to include("inbox_message_path: function(_inbox_id, _id, options)")
     end
     it "should have correct function signature with unordered hash" do
-      should include("inbox_message_attachment_path: function(_inbox_id, _message_id, _id, options)")
+      is_expected.to include("inbox_message_attachment_path: function(_inbox_id, _message_id, _id, options)")
     end
 
     it "routes should be sorted in alphabetical order" do
@@ -26,11 +26,15 @@ describe JsRoutes do
 
   describe ".generate!" do
 
-    let(:name) {  "#{File.dirname(__FILE__)}/../routes.js" }
+    let(:name) { "#{File.dirname(__FILE__)}/../routes.js" }
 
     before(:each) do
       FileUtils.rm_f(name)
       JsRoutes.generate!({:file => name})
+    end
+
+    after(:all) do
+      FileUtils.rm_f("#{File.dirname(__FILE__)}/../routes.js") # let(:name) is not available here
     end
 
     it "should not generate file before initialization" do
@@ -39,18 +43,15 @@ describe JsRoutes do
       if Rails.application.instance_variable_get("@initialized")
         pending
       end
-      expect(File.exists?(name)).to be_false
+      expect(File.exists?(name)).to be_falsey
     end
 
-    after(:all) do
-      FileUtils.rm_f(name)
-    end
   end
 
   describe "compiled javascript asset" do
     subject { ERB.new(File.read("app/assets/javascripts/js-routes.js.erb")).result(binding) }
     it "should have js routes code" do
-      should include("inbox_message_path: function(_inbox_id, _id, options)")
+      is_expected.to include("inbox_message_path: function(_inbox_id, _id, options)")
     end
   end
 end
