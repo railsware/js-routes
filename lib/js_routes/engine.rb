b713dcc59c142d532e7f8b9622633ace802a86c0
Bogdan Gusiev <agresso@gmail.com>
2016-01-26 16:31:56 +0200

    Fix for sprockets 2.0

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -4,10 +4,10 @@ class JsRoutes
   SPROCKETS3 = Gem::Version.new(Sprockets::VERSION) >= Gem::Version.new('3.0.0')
   class Engine < ::Rails::Engine
 
-    config.after_initialize do
-      routes = Rails.root.join('config', 'routes.rb').to_s
+    initializer 'js-routes.dependent_on_routes', after: "sprockets.environment" do
 
       if Rails.application.assets.respond_to?(:register_preprocessor)
+        routes = Rails.root.join('config', 'routes.rb').to_s
         Rails.application.assets.register_preprocessor 'application/javascript', :'js-routes_dependent_on_routes' do |ctx,data|
           ctx.depend_on(routes) if ctx.logical_path == 'js-routes'
           data
