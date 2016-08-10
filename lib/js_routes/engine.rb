cce3fdea6ef4cf0202cc6ebb8d5216698d5be6b9
Bogdan Gusiev <agresso@gmail.com>
2016-08-10 15:44:19 +0300

    Cleanup

--- lib/js_routes/engine.rb
+++ lib/js_routes/engine.rb
@@ -32,7 +32,7 @@ class Engine < ::Rails::Engine
   v2                = Gem::Dependency.new('', ' ~> 2')
   v3                = Gem::Dependency.new('', ' >= 3' ,' < 3.7')
   v37                = Gem::Dependency.new('', ' >= 3.7')
-  sprockets_version = Gem::Version.new(Sprockets::VERSION).release
+  sprockets_version = Gem::Version.new(::Sprockets::VERSION).release
   initializer_args  = case sprockets_version
                         when -> (v) { v2.match?('', v) }
                           { after: "sprockets.environment" }
