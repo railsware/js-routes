5097e7df5574ced65e87a34f317ba8102bac6157
Bogdan Gusiev <agresso@gmail.com>
2024-11-15 20:23:38 +0100

    Remove old code

--- spec/js_routes/options_spec.rb
+++ spec/js_routes/options_spec.rb
@@ -465,12 +465,6 @@ describe JsRoutes, "options" do
     end
 
     context "with url_links option" do
-      around(:each) do |example|
-        ActiveSupport::Deprecation.silence do
-          example.run
-        end
-      end
-
       let(:_options) { { :compact => true, :url_links => true, default_url_options: {host: 'localhost'} } }
       it "should not strip urls" do
         expectjs("Routes.inbox(1)").to eq(test_routes.inbox_path(1))
