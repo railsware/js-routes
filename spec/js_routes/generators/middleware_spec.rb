ca9b7989f5567453ab89375975318a7f8f02ddf6
Jeffrey Baxendale <jbaxendale@usertesting.com>
2026-05-28 13:57:27 -0400

    Remove redundant initializer template spec
    Review called the generator assertion unnecessary. The initializer template itself remains covered by generation behavior, so this only deletes the added content check.

--- spec/js_routes/generators/middleware_spec.rb
+++ spec/js_routes/generators/middleware_spec.rb
@@ -5,10 +5,4 @@ describe JsRoutes::Generators::Middleware do
   it "has correct source_root" do
     expect(JsRoutes::Generators::Middleware.source_root).to eq(gem_root.join('lib/templates').to_s)
   end
-
-  it "sets the undefined query parameter migration option in the generated initializer" do
-    expect(File.read(gem_root.join('lib/templates/initializer.rb'))).to include(
-      "c.omit_undefined_query_parameters = true"
-    )
-  end
 end
