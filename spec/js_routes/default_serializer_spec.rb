40fe3fee7470c7b0a0eabdea0e9b0ec2d62d3fcf
Jeff Baxendale <jbaxendale@usertesting.com>
2026-06-03 15:12:15 -0400

    call missed to_query in default serializer specs

--- spec/js_routes/default_serializer_spec.rb
+++ spec/js_routes/default_serializer_spec.rb
@@ -15,7 +15,7 @@ describe JsRoutes, "#serialize" do
 
   it "should provide this method" do
     expectjs("Routes.serialize({a: 1, b: [2,3], c: {d: 4, e: 5}, f: ''})").to eq(
-      "a=1&b%5B%5D=2&b%5B%5D=3&c%5Bd%5D=4&c%5Be%5D=5&f="
+      {a: 1, b: [2,3], c: {d: 4, e: 5}, f: ''}.to_query
     )
   end
 
@@ -27,7 +27,7 @@ describe JsRoutes, "#serialize" do
         "query.b = 2;",
         "Routes.serialize(query);",
       ].join("\n")
-    ).to eq("a=1&b=2")
+    ).to eq({a:1, b:2}.to_query)
   end
 
   it "serializes undefined object properties as Rails nil by default" do
